58bbb95d5f62f6a3e9041f0df4afd0dadcdd7954
zenglinggui <zenglinggui@pekon.com>
2025-03-12 12:10:20 +0800

    添加信息流展示广告

--- base/src/main/java/com/common/wheel/admanager/InformationFlowManager.java
+++ base/src/main/java/com/common/wheel/admanager/InformationFlowManager.java
@@ -2,6 +2,7 @@ package com.common.wheel.admanager;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import com.blankj.utilcode.util.LogUtils;
@@ -11,6 +12,8 @@ import com.bytedance.sdk.openadsdk.TTAdManager;
 import com.bytedance.sdk.openadsdk.TTAdNative;
 import com.bytedance.sdk.openadsdk.TTFeedAd;
 import com.bytedance.sdk.openadsdk.mediation.ad.MediationAdSlot;
+import com.bytedance.sdk.openadsdk.mediation.ad.MediationExpressRenderListener;
+import com.bytedance.sdk.openadsdk.mediation.manager.MediationNativeManager;
 
 import java.util.List;
 import java.util.Map;
@@ -36,6 +39,7 @@ public class InformationFlowManager {
                  *  2:如果是信息流模板广告，宽度设置为希望的宽度，高度设置为0(0为高度选择自适应参数)
                  */
                 .setImageAcceptedSize(200, 100)
+                .setAdCount(1)//请求广告数量为1到3条 （优先采用平台配置的数量）
                 .setMediationAdSlot(// 聚合广告请求配置
                         new MediationAdSlot.Builder()
                                 .setMuted(false)
@@ -73,7 +77,50 @@ public class InformationFlowManager {
                     } else {
                         //非下载类广告
                     }
-                    splashContainer.addView(mTTFeedAd.getAdView());
+                    mTTFeedAd.uploadDislikeEvent("mediation_dislike_event");
+                    /** 5、展示广告 */
+                    MediationNativeManager manager = mTTFeedAd.getMediationManager();
+                    if (manager != null) {
+                        if (manager.isExpress()) { // --- 模板feed流广告
+                            mTTFeedAd.setExpressRenderListener(new MediationExpressRenderListener() {
+                                @Override
+                                public void onRenderSuccess(View view, float v, float v1, boolean b) {
+                                    if (mTTFeedAd != null) {
+                                        View expressFeedView = mTTFeedAd.getAdView(); // *** 注意不要使用onRenderSuccess参数中的view ***
+//                                        UIUtils.removeFromParent(expressFeedView);
+                                        splashContainer.removeAllViews();
+                                        splashContainer.addView(expressFeedView);
+                                    }
+                                }
+
+                                @Override
+                                public void onRenderFail(View view, String s, int i) {
+
+                                }
+
+                                @Override
+                                public void onAdClick() {
+
+                                }
+
+                                @Override
+                                public void onAdShow() {
+
+                                }
+                            });
+                            mTTFeedAd.render(); // 调用render方法进行渲染，在onRenderSuccess中展示广告
+
+                        } else {                   // --- 自渲染feed流广告
+
+                            // 自渲染广告返回的是广告素材，开发者自己将其渲染成view
+//                            View feedView = FeedAdUtils.getFeedAdFromFeedInfo(mTTFeedAd, this, null, mAdInteractionListener);
+//                            if (feedView != null) {
+//                                UIUtils.removeFromParent(feedView);
+//                                mFeedContainer.removeAllViews();
+//                                mFeedContainer.addView(feedView);
+//                            }
+                        }
+                    }
                 }
             }
         });
