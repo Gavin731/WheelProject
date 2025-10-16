705e9c9f3960e8c5e65b3c0f91378c73eed2d196
zenglinggui <>
2025-10-16 15:48:39 +0800

    添加电话权限

--- socialsecurity/src/main/java/com/rzm/socialsecurity/activity/MainActivity.java
+++ socialsecurity/src/main/java/com/rzm/socialsecurity/activity/MainActivity.java
@@ -531,10 +531,10 @@ public class MainActivity extends MvpActivity<MainPresenter> implements IMainVie
 
     public void requestPermission() {
 //        AdvertisementManager.getInstance().requestPermissionIfNecessary(this);
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
-//                != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
-//        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+        }
 //        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
 //                != PackageManager.PERMISSION_GRANTED) {
 //            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
